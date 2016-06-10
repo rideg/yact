ee0f276fce3e2488c3d8b30b5c398466e306cb8e
rideg <sandor.rideg@gmail.com>
2016-06-09 20:01:13 -0700

    Rename lib files: add .sh suffix

--- todo.sh
+++ todo.sh
@@ -10,10 +10,10 @@ test ! -e $YACT_DIR && mkdir -p $YACT_DIR
 . ./config
 
 test -e $YACT_DIR/config && . $YACT_DIR/config
-. lib/util
-. lib/colors
-. lib/show
-. lib/done
+. lib/util.sh
+. lib/colors.sh
+. lib/show.sh
+. lib/done.sh
 
 if [ "$1" = '-l' ]; then
  if [ "$2" = "--new" -o "$2" = "-n" ]; then
