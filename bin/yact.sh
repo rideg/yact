e23927ea2cef28552e983fc597fa28da9857a9e4
rideg <sandor.rideg@gmail.com>
2016-06-11 10:30:15 -0700

    Implements show list; fix delete list

--- bin/yact.sh
+++ bin/yact.sh
@@ -16,22 +16,29 @@ test -e $YACT_DIR/config && . $YACT_DIR/config
 . lib/lists.sh
 . lib/tasks.sh
 
+test -e $YACT_DIR/.last && . $YACT_DIR/.last
+FILE=$YACT_DIR/$TODO_FILE
+
 if [ "$1" = '-l' ]; then
- if [ "$2" = "--new" -o "$2" = "-n" ]; then
+ if [ "$2" = "--add" -o "$2" = "-a" ]; then
   shift 2
   new_list $* 
- elif [ "$2" = "--switch" -o "$2" = "-s" ]; then
-  switch_list $3
  fi
 fi
-
-test -e $YACT_DIR/.last && . $YACT_DIR/.last
 
 if [ -z $TODO_FILE ]; then
   fatal "No todo list has been selected, please select/create one."
 fi
 
-FILE=$YACT_DIR/$TODO_FILE
+if [ "$1" = '-l' ]; then
+ if [ "$2" = "--switch" -o "$2" = "-s" ]; then
+  switch_list $3
+ elif [ "$2" = "--delete" ]; then
+  delete_list $3
+ else
+  show_list
+ fi 
+fi
 
 test $# -eq 0 && show_tasks
 test "$1" = '--done' -o "$1" = '-d' && set_done "$2" 1
--- lib/lists.sh
+++ lib/lists.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
 
+update_file_and_show() {
+  . $YACT_DIR/.last
+  FILE="$YACT_DIR/$TODO_FILE"
+  show_list
+}
+
 new_list() {
   if [ -z "$*" ]; then
     fatal "Please provide description for the new list." 
@@ -11,6 +17,7 @@ new_list() {
   file_name="_$(timestamp).txt"
   printf "$*\n\n" > $YACT_DIR/$file_name
   printf 'TODO_FILE=%s\n' $file_name > $YACT_DIR/.last
+  update_file_and_show 
 }
 
 switch_list() {
@@ -19,8 +26,58 @@ switch_list() {
    fi
    file_name=_${1}.txt
    if [ ! -f $YACT_DIR/$file_name ]; then
-    fatal "Nonexisting list id $1"
+    fatal "Non-existing list id $1"
    fi
    printf 'TODO_FILE=%s\n' $file_name > $YACT_DIR/.last
+   update_file_and_show 
 }
 
+delete_list() {
+  local to_delete=$FILE 
+  if [ ! -z $1 ]; then
+    to_delete="$YACT_DIR/_${1}.txt"
+  fi
+  rm -f $to_delete & > /dev/null
+  if [ "$to_delete" = "$FILE" ]; then
+    local next_file=$(ls -ur $YACT_DIR/_*.txt 2> /dev/null | head -n1)
+    if [ -z $next_file ]; then
+      rm $YACT_DIR/.last
+    else
+      printf 'TODO_FILE=%s\n' $(basename $next_file) > $YACT_DIR/.last
+      update_file_and_show
+    fi
+  fi
+}
+
+file_header_with_info() {
+  awk '\
+     BEGIN {\
+       done=0; 
+       count=0;
+       FS=";";
+     }\
+     {\
+        if (NR == 1) { \
+         printf $0\
+        };\
+        if(NR > 2) {\
+         count++; \
+         if ($3 == "1") {\
+           done++;\
+          }\
+        }\
+     }; END {printf " (%d/%d)", count, done; }' $1
+}
+
+show_list() {
+  printf "\n $(color 'You have the following lists' $bold $underline):\n\n"
+  for actual_file in $(ls -ur $YACT_DIR/_*.txt); do
+    if [ "$actual_file" = "$FILE" ]; then
+     indicator='*'
+    else
+     indicator=''
+    fi
+    printf ' %-1s %s\t%s\n' "$indicator" $(echo $actual_file | sed -e 's:^.*/_\(.*\)\.txt$:\1:') "$(file_header_with_info $actual_file)"
+  done
+  printf '\n'
+}
